d98f69e360ae7820da691f31230e5577c406b87b
itmayziii <tommymay37@gmail.com>
2019-06-26 14:46:25 -0400

    test(Tests): Added remaining tests

---
+++ src/fake-data.spec.ts
@@ -0,0 +1,233 @@
+// Data taken directly from a real Gatsby build
+
+export const scriptsData = [
+  { rel: 'preload', name: 'webpack-runtime-d53148f285188e47861c.js' },
+  { rel: 'preload', name: 'styles-fc78e7c610cb29615a01.js' },
+  { rel: 'preload', name: '1-aff5fb503cf05cb46096.js' },
+  { rel: 'preload', name: 'app-9df10a4ee487889eb94f.js' },
+  { rel: 'preload', name: 'component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js' },
+  { rel: 'preload', name: '3-026f7b22973cd8a93202.js' }
+]
+
+export const headComponentsData = [
+  {
+    type: 'style',
+    key: null,
+    ref: null,
+    props:
+    {
+      'data-href': '/styles.4fefa8df208ade30a696.css',
+      dangerouslySetInnerHTML: 'p{color:red}'
+    },
+    _owner: null
+  },
+  {
+    type: 'meta',
+    key: 'generator-2.6.0',
+    ref: null,
+    props: { name: 'generator', content: 'Gatsby 2.6.0' },
+    _owner: null
+  },
+  [{
+    type: 'title',
+    key: 'Your Pickup Request Has Been Received | Budget Dumpster ',
+    ref: null,
+    props: {},
+    _owner: null
+  }],
+  [{
+    type: 'link',
+    key: '0',
+    ref: null,
+    props: {},
+    _owner: null
+  },
+    {
+      type: 'link',
+      key: '1',
+      ref: null,
+      props: {},
+      _owner: null
+    }],
+  [{
+    type: 'meta',
+    key: '0',
+    ref: null,
+    props: {},
+    _owner: null
+  }],
+  {
+    type: 'script',
+    key: 'plugin-google-tagmanager',
+    ref: null,
+    props: { dangerouslySetInnerHTML: {} },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: 'invoca',
+    ref: null,
+    props: { dangerouslySetInnerHTML: {} },
+    _owner: null
+  },
+  [{
+    type: 'title',
+    key: '',
+    ref: null,
+    props: {},
+    _owner: null
+  }],
+  {
+    type: 'link',
+    key: 'styles-fc78e7c610cb29615a01.js',
+    ref: null,
+    props:
+    {
+      as: 'script',
+      rel: 'preload',
+      href: '/styles-fc78e7c610cb29615a01.js'
+    },
+    _owner: null
+  },
+  {
+    type: 'link',
+    key: 'component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js',
+    ref: null,
+    props:
+    {
+      as: 'script',
+      rel: 'preload',
+      href: '/component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js'
+    },
+    _owner: null
+  },
+  {
+    type: 'link',
+    key: '3-026f7b22973cd8a93202.js',
+    ref: null,
+    props:
+    {
+      as: 'script',
+      rel: 'preload',
+      href: '/3-026f7b22973cd8a93202.js'
+    },
+    _owner: null
+  },
+  {
+    type: 'link',
+    key: '1-aff5fb503cf05cb46096.js',
+    ref: null,
+    props:
+    {
+      as: 'script',
+      rel: 'preload',
+      href: '/1-aff5fb503cf05cb46096.js'
+    },
+    _owner: null
+  },
+  {
+    type: 'link',
+    key: 'app-9df10a4ee487889eb94f.js',
+    ref: null,
+    props:
+    {
+      as: 'script',
+      rel: 'preload',
+      href: '/app-9df10a4ee487889eb94f.js'
+    },
+    _owner: null
+  },
+  {
+    type: 'link',
+    key: 'webpack-runtime-d53148f285188e47861c.js',
+    ref: null,
+    props:
+    {
+      as: 'script',
+      rel: 'preload',
+      href: '/webpack-runtime-d53148f285188e47861c.js'
+    },
+    _owner: null
+  },
+  {
+    type: 'link',
+    key: '/static/d/991/path---schedule-a-pickup-thank-you-php-7-b-9-bc7-0SUcWyAf8ecbYDsMhQkEfPzV8.json',
+    ref: null,
+    props:
+    {
+      as: 'fetch',
+      rel: 'preload',
+      href: '/static/d/991/path---schedule-a-pickup-thank-you-php-7-b-9-bc7-0SUcWyAf8ecbYDsMhQkEfPzV8.json',
+      crossOrigin: 'use-credentials'
+    },
+    _owner: null
+  }
+]
+
+export const postBodyComponentsData = [
+  {
+    type: 'script',
+    key: 'google-auto-complete',
+    ref: null,
+    props: { defer: true, src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDZJcMmCmwtkCGJqLQMzy75fVwnwcFYt50&libraries=places' },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: 'script-loader',
+    ref: null,
+    props:
+    {
+      id: 'gatsby-script-loader',
+      dangerouslySetInnerHTML: '/*<![CDATA[*/window.page={"componentChunkName":"component---src-templates-page-js","jsonName":"index","path":"/"};window.dataPath="13/path---index-6a9-yTGkIonQ3f8vkIpEPxA6hAnik";/*]]>*/'
+    },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: 'chunk-mapping',
+    ref: null,
+    props:
+    {
+      id: 'gatsby-chunk-mapping',
+      dangerouslySetInnerHTML: '/*<![CDATA[*/window.___chunkMapping={"app":["/app-5829768fe8bcbf2b87f4.js"],"component---src-templates-page-js":["/component---src-templates-page-js-f59d0b13f3aa0b9b41ef.js"],"component---src-pages-about-employment-thank-you-js":["/component---src-pages-about-employment-thank-you-js-377ce6db59535f5b52c2.js"],"component---src-pages-about-employment-driver-js":["/component---src-pages-about-employment-driver-js-380c57905162f024afb0.js"],"component---src-pages-about-employment-index-js":["/component---src-pages-about-employment-index-js-920c728c069966d68988.js"],"component---src-pages-about-employment-ops-manager-js":["/component---src-pages-about-employment-ops-manager-js-b8ffdc3d88f9efde9d81.js"],"component---src-pages-client-js":["/component---src-pages-client-js-9d548b11b5bd39a7b97d.js"],"pages-manifest":["/pages-manifest-51f7e80dd58503a5d560.js"],"modules-calculator":["/modules-calculator-96aac127fc2ba27a491b.js"],"modules-calculator-js":[],"modules-card-list":["/modules-card-list-2f3e57c4242a3756a909.js"],"modules-card-list-js":[],"modules-copy-copy-copy-copy":["/modules-copy-copy-copy-copy-46712846b039d077395e.js"],"modules-copy-copy-copy-copy-js":[],"modules-copy-copy-copy-copy-module-css":[],"modules-copy-form":["/modules-copy-form-9311b7ff7a20f8682256.js"],"modules-copy-form-js":[],"modules-copy-icon-cards":["/modules-copy-icon-cards-af4ea6fc6d9e6f3ca0e1.js"],"modules-copy-icon-cards-js":[],"modules-copy-image":["/modules-copy-image-369196609c6cd484deb8.js"],"modules-copy-image-js":[],"modules-copy-table":["/modules-copy-table-c05cb9f18062985d73b3.js"],"modules-copy-table-js":[],"modules-copy-video":["/modules-copy-video-dae563f4385439a76f22.js"],"modules-copy-video-js":[],"modules-cta-bar":["/modules-cta-bar-c1cd1260439e17f5ddf5.js"],"modules-cta-bar-js":[],"modules-dumpsters-difference":["/modules-dumpsters-difference-3b5dba285944abac38e3.js"],"modules-dumpsters-difference-js":[],"modules-footer-cta-bar":["/modules-footer-cta-bar-2c568fac3b783cd35010.js"],"modules-footer-cta-bar-js":[],"modules-full-width-copy":["/modules-full-width-copy-2c42bd33d3d8cfb11d1b.js"],"modules-full-width-copy-js":[],"modules-full-width-table":["/modules-full-width-table-510d10a1ea898d68dd6f.js"],"modules-full-width-table-js":[],"modules-hero-hero":["/modules-hero-hero-43ad8a078588263fd307.js"],"modules-hero-hero-js":[],"modules-hero-hero-module-css":[],"modules-homepage-hero-homepage-hero":["/modules-homepage-hero-homepage-hero-60bd8703411db15a19d6.js"],"modules-homepage-hero-homepage-hero-js":[],"modules-homepage-hero-homepage-hero-module-css":[],"modules-icon-list-long-desc":["/modules-icon-list-long-desc-a688a00b9d36ae163455.js"],"modules-icon-list-long-desc-js":[],"modules-icon-list-short-desc":["/modules-icon-list-short-desc-22849a17f821c46fa693.js"],"modules-icon-list-short-desc-js":[],"modules-image-links-list-image-links-list":["/modules-image-links-list-image-links-list-fb9adf6181bf23ac06eb.js"],"modules-image-links-list-image-links-list-js":[],"modules-image-links-list-image-links-list-module-css":[],"modules-image-list":["/modules-image-list-ebf52adc49c6f54509f4.js"],"modules-image-list-js":[],"modules-image-segment-list":["/modules-image-segment-list-1dd4311bf2c32f3cb61b.js"],"modules-image-segment-list-js":[],"modules-lightbox-form":["/modules-lightbox-form-0c6aaf2b4bf6abdc7b2b.js"],"modules-lightbox-form-js":[],"modules-local-accepted-materials-local-accepted-materials":["/modules-local-accepted-materials-local-accepted-materials-55668892cfca9192d40c.js"],"modules-local-accepted-materials-local-accepted-materials-js":[],"modules-local-accepted-materials-local-accepted-materials-module-css":[],"modules-local-hero-local-hero":["/modules-local-hero-local-hero-fa35eb7cb0c8cdded2c5.js"],"modules-local-hero-local-hero-js":[],"modules-local-hero-local-hero-module-css":[],"modules-local-product-card-local-product-card":["/modules-local-product-card-local-product-card-9a6253d778167a28dff5.js"],"modules-local-product-card-local-product-card-js":[],"modules-local-product-card-local-product-card-module-css":[],"modules-local-product-card-product-card":["/modules-local-product-card-product-card-08fb5699760f09b625a1.js"],"modules-local-product-card-product-card-js":[],"modules-local-quick-links-local-quick-links":["/modules-local-quick-links-local-quick-links-65fc373e18f32b0ba0cb.js"],"modules-local-quick-links-local-quick-links-js":[],"modules-local-quick-links-local-quick-links-module-css":[],"modules-locations-locations":["/modules-locations-locations-177e0d58b2228602446d.js"],"modules-locations-locations-js":[],"modules-locations-locations-module-css":[],"modules-logo-banner":["/modules-logo-banner-e98e941dd07c0274531e.js"],"modules-logo-banner-js":[],"modules-map-copy":["/modules-map-copy-5a9d88882f4a780342b1.js"],"modules-map-copy-js":[],"modules-module-prop-types":["/modules-module-prop-types-7a0573f9b2f8655cc14f.js"],"modules-module-prop-types-js":[],"modules-price-search-cta":["/modules-price-search-cta-87b6b4b5934c52dd7bf1.js"],"modules-price-search-cta-js":[],"modules-product-cards-main-product-cards-main":["/modules-product-cards-main-product-cards-main-56d8f3ae32bcdf5a958d.js"],"modules-product-cards-main-product-cards-main-js":[],"modules-product-cards-main-product-cards-main-module-css":[],"modules-product-cards-stacked-product-cards-stacked":["/modules-product-cards-stacked-product-cards-stacked-441699a9027e9fb06ab4.js"],"modules-product-cards-stacked-product-cards-stacked-js":[],"modules-product-cards-stacked-product-cards-stacked-module-css":[],"modules-project-grid-project-grid":["/modules-project-grid-project-grid-9e7b9b6e923abfe4be1c.js"],"modules-project-grid-project-grid-js":[],"modules-project-grid-project-grid-module-css":[],"modules-quick-links-menu":["/modules-quick-links-menu-87539370ed353186ca71.js"],"modules-quick-links-menu-js":[],"modules-single-product-feature-single-product-feature":["/modules-single-product-feature-single-product-feature-9401eed3efef826604ad.js"],"modules-single-product-feature-single-product-feature-js":[],"modules-tabbed-content-tabbed-content":["/modules-tabbed-content-tabbed-content-e55058bc437c254c69e1.js"],"modules-tabbed-content-tabbed-content-js":[],"modules-tabbed-content-tabbed-content-module-css":[],"modules-testimonials-testimonials":["/modules-testimonials-testimonials-3cbb0c977c97bf987e66.js"],"modules-testimonials-testimonials-js":[],"modules-testimonials-testimonials-module-css":[]};/*]]>*/'
+    },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: '/webpack-runtime-d53148f285188e47861c.js',
+    ref: null,
+    props: { src: '/webpack-runtime-d53148f285188e47861c.js', async: true },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: '/app-9df10a4ee487889eb94f.js',
+    ref: null,
+    props: { src: '/app-9df10a4ee487889eb94f.js', async: true },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: '/1-aff5fb503cf05cb46096.js',
+    ref: null,
+    props: { src: '/1-aff5fb503cf05cb46096.js', async: true },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: '/3-026f7b22973cd8a93202.js',
+    ref: null,
+    props: { src: '/3-026f7b22973cd8a93202.js', async: true },
+    _owner: null
+  },
+  {
+    type: 'script',
+    key: '/component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js',
+    ref: null,
+    props:
+        { src: '/component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js', async: true },
+    _owner: null
+  }
+]
--- src/gatsby-ssr.spec.ts
+++ src/gatsby-ssr.spec.ts
@@ -1,5 +1,7 @@
-import { onPreRenderHTML, onRenderBody, Script } from './gatsby-ssr'
+import { onPreRenderHTML, onRenderBody } from './gatsby-ssr'
+
 import Spy = jasmine.Spy
+import { headComponentsData, postBodyComponentsData, scriptsData } from './fake-data.spec'
 
 describe('gatsby-ssr.js', () => {
 
@@ -10,15 +12,59 @@ describe('gatsby-ssr.js', () => {
   })
 
   describe('onPreRenderHTML', () => {
-    let replaceHeadComponentsSpy: Spy<any>
-    let replacePostBodyComponentsSpy: Spy<any>
+    let replaceHeadComponentsSpy: Spy
+    let replacePostBodyComponentsSpy: Spy
     beforeEach(() => {
-      replaceHeadComponentsSpy = jasmine.createSpy('replaceHeadComponents')
-      replacePostBodyComponentsSpy = jasmine.createSpy('replacePostBodyComponents')
+      replaceHeadComponentsSpy = jasmine.createSpy<any>('replaceHeadComponents')
+      replacePostBodyComponentsSpy = jasmine.createSpy<any>('replacePostBodyComponents')
     })
 
     it('does not remove non react components from the head, (checks for props)', function () {
-      const fakeHeadComponents = [
+      function getHeadComponents () {
+        return [headComponentsData[2], headComponentsData[3]]
+      }
+      function getPostBodyComponents () {
+        return []
+      }
+
+      onPreRenderHTML({ getHeadComponents, replaceHeadComponents: replaceHeadComponentsSpy, getPostBodyComponents, replacePostBodyComponents: replacePostBodyComponentsSpy })
+      expect(replaceHeadComponentsSpy).toHaveBeenCalledWith([headComponentsData[2], headComponentsData[3]])
+    })
+
+    it('should remove static files like JSON from the head as these files are always added by Gatsby', () => {
+      onRenderBody({ scripts: [] })
+      function getHeadComponents () {
+        return [headComponentsData[0], headComponentsData[1], headComponentsData[14]]
+      }
+      function getPostBodyComponents () {
+        return []
+      }
+
+      onPreRenderHTML({ getHeadComponents, replaceHeadComponents: replaceHeadComponentsSpy, getPostBodyComponents, replacePostBodyComponents: replacePostBodyComponentsSpy })
+      expect(replaceHeadComponentsSpy).toHaveBeenCalledWith([headComponentsData[0], headComponentsData[1]])
+    })
+
+    it('should remove preload scripts from the head that are called out by Gatsby during onRenderBody', () => {
+      onRenderBody({ scripts: scriptsData })
+
+      function getHeadComponents () {
+        return [
+          headComponentsData[0], headComponentsData[1], headComponentsData[8], headComponentsData[9], headComponentsData[10], headComponentsData[11],
+          headComponentsData[12], headComponentsData[13], headComponentsData[14]
+        ]
+      }
+      function getPostBodyComponents () {
+        return []
+      }
+
+      onPreRenderHTML({ getHeadComponents, replaceHeadComponents: replaceHeadComponentsSpy, getPostBodyComponents, replacePostBodyComponents: replacePostBodyComponentsSpy })
+      expect(replaceHeadComponentsSpy).toHaveBeenCalledTimes(1)
+      expect(replaceHeadComponentsSpy.calls.argsFor(0)[0].length).toEqual(2)
+      expect(replaceHeadComponentsSpy).toHaveBeenCalledWith([headComponentsData[0], headComponentsData[1]])
+    })
+
+    it('does not remove non react components from the body, (checks for props)', function () {
+      const fakeBodyComponents = [
         {
           type: 'link',
           key: 'styles-module1.css'
@@ -29,89 +75,43 @@ describe('gatsby-ssr.js', () => {
         }
       ]
       function getHeadComponents () {
-        return fakeHeadComponents
+        return []
       }
       function getPostBodyComponents () {
-        return []
+        return fakeBodyComponents
       }
 
       onPreRenderHTML({ getHeadComponents, replaceHeadComponents: replaceHeadComponentsSpy, getPostBodyComponents, replacePostBodyComponents: replacePostBodyComponentsSpy })
-      expect(replaceHeadComponentsSpy).toHaveBeenCalledWith(fakeHeadComponents)
+      expect(replacePostBodyComponentsSpy).toHaveBeenCalledWith(fakeBodyComponents)
     })
 
-    it('should remove static files like JSON from the head as these files are always added by Gatsby', () => {
+    it('should remove special Gatsby scripts', () => {
       onRenderBody({ scripts: [] })
-      const fakeHeadComponents = [
-        {
-          type: 'link',
-          key: 'some-component.json',
-          ref: null,
-          props: {
-            as: 'script',
-            rel: 'preload',
-            href: '/static/d/some-component.json'
-          }
-        },
-        {
-          type: 'link',
-          key: 'styles-module1.css'
-        }
-      ]
       function getHeadComponents () {
-        return fakeHeadComponents
+        return []
       }
       function getPostBodyComponents () {
-        return []
+        return [postBodyComponentsData[0], postBodyComponentsData[1], postBodyComponentsData[2]]
       }
 
       onPreRenderHTML({ getHeadComponents, replaceHeadComponents: replaceHeadComponentsSpy, getPostBodyComponents, replacePostBodyComponents: replacePostBodyComponentsSpy })
-      expect(replaceHeadComponentsSpy).toHaveBeenCalledWith([fakeHeadComponents[1]])
+      expect(replacePostBodyComponentsSpy).toHaveBeenCalledWith([postBodyComponentsData[0]])
     })
 
-    it('should remove preload scripts from the head that are called out by Gatsby during onRenderBody', () => {
-      const gatsbyGeneratedScripts: Script[] = [
-        { name: 'app.a6d963769d2dc276611e.js', rel: 'preload' },
-        { name: 'component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js', rel: 'preload' }
-      ]
-      onRenderBody({ scripts: gatsbyGeneratedScripts })
+    it('should remove preload scripts from the body that are called out by Gatsby during onRenderBody', () => {
+      onRenderBody({ scripts: scriptsData })
 
-      const fakeHeadComponents = [
-        {
-          type: 'link',
-          key: 'app.a6d963769d2dc276611e.js',
-          ref: null,
-          props: {
-            as: 'script',
-            rel: 'preload',
-            href: '/app.a6d963769d2dc276611e.js'
-          }
-        },
-        {
-          type: 'link',
-          key: 'component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js',
-          ref: null,
-          props: {
-            as: 'script',
-            rel: 'preload',
-            href: '/component---src-pages-schedule-a-pickup-php-js-a6d963769d2dc276611e.js'
-          }
-        },
-        {
-          type: 'link',
-          key: 'styles-module1.css'
-        }
-      ]
       function getHeadComponents () {
-        return fakeHeadComponents
+        return []
       }
       function getPostBodyComponents () {
-        return []
+        return [postBodyComponentsData[0], postBodyComponentsData[3], postBodyComponentsData[4], postBodyComponentsData[5], postBodyComponentsData[6], postBodyComponentsData[7]]
       }
 
       onPreRenderHTML({ getHeadComponents, replaceHeadComponents: replaceHeadComponentsSpy, getPostBodyComponents, replacePostBodyComponents: replacePostBodyComponentsSpy })
-      expect(replaceHeadComponentsSpy).toHaveBeenCalledWith([fakeHeadComponents[2]])
+      expect(replacePostBodyComponentsSpy).toHaveBeenCalledTimes(1)
+      expect(replacePostBodyComponentsSpy.calls.argsFor(0)[0].length).toEqual(1)
+      expect(replacePostBodyComponentsSpy).toHaveBeenCalledWith([postBodyComponentsData[0]])
     })
-
   })
-
 })
