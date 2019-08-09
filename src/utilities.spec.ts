e2824089ec120cc6fc0121ce565d5c82623c9d71
itmayziii <tommymay37@gmail.com>
2019-08-08 22:28:39 -0400

    style(ESLint): Migrated to ESLint over TSLint as TSLint is deprecated.
    https://medium.com/palantir/tslint-in-2019-1a144c2317a9

--- src/utilities.spec.ts
+++ src/utilities.spec.ts
@@ -1,7 +1,7 @@
 import { checkPathExclusion } from './utilities'
 
-describe('checkPathExclusion', () => {
-  it('does the pathname match any of the exclusions', () => {
+describe('checkPathExclusion', (): void => {
+  it('does the pathname match any of the exclusions', (): void => {
     expect(checkPathExclusion('/client/omegablerns', { excludePaths: /\/client/ })).toBeTruthy()
     expect(checkPathExclusion('/my-cool-page', { excludePaths: /\/my-cool-page/ })).toBeTruthy()
     expect(checkPathExclusion('/about/blerns', { excludePaths: /(\/client)|(\/tacos)/ })).toBeFalsy()
