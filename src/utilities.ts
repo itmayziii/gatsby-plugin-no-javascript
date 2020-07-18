47053a108d2bec1d6a3909f7175476a16d67b62a
FraserMuir <frasermuir14@gmail.com>
2020-07-18 19:04:45 +0100

    fix lint error

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,5 @@
 import { PluginOptions } from './gatsby-ssr'
-import { relative, isAbsolute } from 'path';
+import { relative, isAbsolute } from 'path'
 
 export function checkPathExclusion (pathname: string, pluginOptions: PluginOptions): boolean {
   if (!pluginOptions.excludePaths) return false
@@ -11,6 +11,6 @@ export function checkPathExclusion (pathname: string, pluginOptions: PluginOptio
  * Checks if a given pathname is a child of a given dirname
  */
 export function isChildOfDirectory (dirname: string, pathname: string): boolean {
-  const resolution = relative(dirname, pathname);
-  return !!(resolution && !resolution.startsWith('..') && !isAbsolute(resolution));
+  const resolution = relative(dirname, pathname)
+  return !!(resolution && !resolution.startsWith('..') && !isAbsolute(resolution))
 }
