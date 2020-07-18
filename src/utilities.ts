e2d1f7a814995a66ba32509c0ec263230f413cdb
FraserMuir <frasermuir14@gmail.com>
2020-07-18 19:00:15 +0100

    test: add tests for isChildOfDirectory utility

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,7 +1,16 @@
 import { PluginOptions } from './gatsby-ssr'
+import { relative, isAbsolute } from 'path';
 
 export function checkPathExclusion (pathname: string, pluginOptions: PluginOptions): boolean {
   if (!pluginOptions.excludePaths) return false
 
   return RegExp(pluginOptions.excludePaths).test(pathname)
+}
+
+/**
+ * Checks if a given pathname is a child of a given dirname
+ */
+export function isChildOfDirectory (dirname: string, pathname: string): boolean {
+  const resolution = relative(dirname, pathname);
+  return !!(resolution && !resolution.startsWith('..') && !isAbsolute(resolution));
 }
